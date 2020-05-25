4a60579824d301c71178e93a427ed489e11382b4
chinghoi <chinghoi@chinghoideMacBook-Pro.local>
2020-05-25 11:50:03 +0800

    init

---
+++ FYTool/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FYTool
+//
+//  Created by chinghoi on 2020/5/25.
+//  Copyright © 2020 chinghoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
